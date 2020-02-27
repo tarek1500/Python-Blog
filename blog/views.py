84da69f53dbd085535a3b9ec675b21d11745efb3
Tarek <tarek_mohammed2@yahoo.com>
2020-02-28 00:32:11 +0200

    Fix redirect bugs

--- blog/views.py
+++ blog/views.py
@@ -40,7 +40,7 @@ def categoryPosts(request, cat):
 def searchPost(request):
     queryset= request.GET.get("query")
     if queryset:
-        queryset_list=Post.objects.filter(Q(title__icontains=queryset)| Q(tag__tag__icontains=queryset))
+        queryset_list=Post.objects.filter(Q(title__icontains=queryset) | Q(tag__name__icontains=queryset))
         context = {'posts':queryset_list}
         return render(request,'posts.html',context)
     else:
@@ -57,13 +57,13 @@ def showPostsByTags(request, id):
 def showpost(request, num):
     is_liked=None
     post = Post.objects.get(id=num)
-    like = Like.objects.filter(post_id=post)
+    like = Like.objects.filter(post=post)
 
     post_likes = like.filter(like = True).count()
     post_dislikes = like.filter(like = False).count()
 
     if request.user.is_authenticated:
-        like = Like.objects.filter(post_id=post, userId=request.user)
+        like = Like.objects.filter(post=post, user=request.user)
 
         if like.exists():
             if like.get().like == True:
@@ -71,7 +71,7 @@ def showpost(request, num):
             else:
                 is_liked=False
 
-    comments=Comment.objects.filter(post_id=post.id , reply=None).order_by('-id')
+    comments=Comment.objects.filter(post=post.id , reply=None).order_by('-id')
     if request.method =='POST':
         comment_form=CommentForm(request.POST or None)
         if comment_form.is_valid():
@@ -80,7 +80,7 @@ def showpost(request, num):
             comment_qs=None
             if reply_id:
                 comment_qs=Comment.objects.get(id=reply_id)
-            comment=Comment.objects.create(post_id=post,user_id=request.user , content=content , reply=comment_qs)
+            comment=Comment.objects.create(post=post, user=request.user , content=content , reply=comment_qs)
             comment.save()
             comment_form = CommentForm()
     else : 
@@ -90,15 +90,15 @@ def showpost(request, num):
 
 @login_required
 def like(request,num):
-    if not Like.objects.filter(post_id=num, userId=request.user.id).exists():
+    if not Like.objects.filter(post=num, user=request.user.id).exists():
         post = Post.objects.get(id=num)
 
         if request.POST.get('like') == '1':
-            Like.objects.create(post_id=post, userId=request.user, like = True)
+            Like.objects.create(post=post, user=request.user, like = True)
         else:
-            Like.objects.create(post_id=post, userId=request.user, like = False)
+            Like.objects.create(post=post, user=request.user, like = False)
 
-        like = Like.objects.filter(post_id=post, like = 0)
+        like = Like.objects.filter(post=post, like = 0)
 
         if like.count() >= 10:
             like.delete()
